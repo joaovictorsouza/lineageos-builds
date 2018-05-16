ea5e4b3d56ba2ccafaea3393a8121daa873744ca
Salvian Reynaldi <salvian@traveloka.com>
2018-05-16 13:21:29 +0700

    swap wrong switch

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ git config --global user.name "Salvian Reynaldi"
 git config --global user.email "salvianreynaldi@gmail.com"
 git config --global color.ui true
 repo init -u git://github.com/LineageOS/android.git -b ${LINEAGE_BRANCH} -q
-wget -PN .repo/local_manifests/ ${LINEAGE_LOCAL_MANIFEST_XML}
+wget -NP .repo/local_manifests/ ${LINEAGE_LOCAL_MANIFEST_XML}
 repo sync -q --no-clone-bundle --no-tags
 ls -al device/xiaomi/jason/
 ls -al kernel/xiaomi/sdm660/
